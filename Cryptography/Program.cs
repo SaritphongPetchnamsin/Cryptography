4f65c7d662462cc42d8eee7740d8c25d673edcd5
Duan TheHealer <saritphong24300@gmail.com>
2022-04-06 21:20:28 +0700

    Add project files.

---
+++ Cryptography/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace Cryptography // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path= ConfigurationSettings.AppSettings.Get("FileString");
+            string strfile = File.ReadAllText(path);
+            ConvertFiletobit confile = new ConvertFiletobit();
+            confile.convertstringtobit(strfile);
+            Findprime prime = new Findprime(confile.stringbit);
+        }
+    }
+}
